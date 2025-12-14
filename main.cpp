baab0e7da04f9b04ea7303e48d9d6a6fc6a3023d
Lo3wer <leozhang226@gmail.com>
2025-12-14 03:36:44 -0800

    Made some cool rainbow triangles and renderings

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "tgaimage.h"
 #include "model.h"
 
-constexpr int width  = 1280;
-constexpr int height = 1280;
+constexpr int width  = 128;
+constexpr int height = 128;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -42,20 +42,23 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
     int maxx = std::max(std::max(ax, bx), cx);
     int maxy = std::max(std::max(ay, by), cy);
     float area = signedTriangleArea(ax, ay, bx, by, cx, cy);
-    if(area < 1.0) {
-        return;
-    }
+    // if(area < 1.0) {
+    //     return;
+    // }
     #pragma omp parallel for
     for (int x=minx; x<=maxx; x++) {
         for (int y=miny; y<=maxy; y++) {
             float alpha = signedTriangleArea(x, y, bx, by, cx, cy) / area;
             float beta  = signedTriangleArea(ax, ay, x, y, cx, cy) / area;
             float gamma = signedTriangleArea(ax, ay, bx, by, x, y) / area;
-            // float alpha = signedTriangleArea(cx, cy, bx, by, x, y) / area;
-            // float beta  = signedTriangleArea(cx, cy, x, y, ax, ay) / area;
-            // float gamma = signedTriangleArea(x, y, bx, by, ax, ay) / area;
-            if (alpha >= 0.0 && beta >= 0.0 && gamma >= 0.0) {
-                framebuffer.set(x, y, color);
+            TGAColor combinedColor = {
+                alpha * red[0] + beta * green[0] + gamma * blue[0],
+                alpha * red[1] + beta * green[1] + gamma * blue[1],
+                alpha * red[2] + beta * green[2] + gamma * blue[2],
+                255
+            };
+            if (alpha >= 0.0 && beta >= 0.0 && gamma >= 0.0 && (alpha <= 0.1 || beta <= 0.1 || gamma <= 0.1)) {
+                framebuffer.set(x, y, combinedColor);
             }
         }
     }
@@ -70,18 +73,23 @@ std::pair<int, int> project(const vec3& v) {
 int main(int argc, char** argv) {
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    Model model("african_head.obj");
+    // Model model("african_head.obj");
 
-    for (int i=0; i<model.nfaces(); i++) {
-        auto [ax, ay] = project(model.vert(i, 0));
-        auto [bx, by] = project(model.vert(i, 1));
-        auto [cx, cy] = project(model.vert(i, 2));
-        TGAColor rnd;
-        for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
-        triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
-    }
+    // for (int i=0; i<model.nfaces(); i++) {
+    //     auto [ax, ay] = project(model.vert(i, 0));
+    //     auto [bx, by] = project(model.vert(i, 1));
+    //     auto [cx, cy] = project(model.vert(i, 2));
+    //     TGAColor rnd;
+    //     for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
+    //     triangle(ax, ay, bx, by, cx, cy, framebuffer, rnd);
+    // }
 
-    framebuffer.write_tga_file("renderingWithCulling.tga");
+    // framebuffer.write_tga_file("renderingWithCulling.tga");
+    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
+    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
+    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
+    framebuffer.write_tga_file("rainbowLines.tga");
+
     return 0;
 }
 
