7d0f7f294290d3d1e6db71bfe27e34f9afabf743
Lo3wer <leozhang226@gmail.com>
2026-01-09 14:48:34 -0800

    resolved compilation issues within the geometry.h file

--- geometry.h
+++ geometry.h
@@ -90,63 +90,65 @@ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     return {v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x};
 }
 
-template<int rows, int cols> struct mat {
-    vec<cols> data[rows];
-    vec<cols>& operator[](const int i)       { assert(i>=0 && i<rows); return data[i]; }
-    const vec<cols>& operator[](const int i) const { assert(i>=0 && i<rows); return data[i]; }
+template <int n> struct determinant;
+
+template<int nrows, int ncols> struct mat {
+    vec<ncols> rows[nrows];
+    vec<ncols>& operator[](const int i) { assert(i>=0 && i<nrows); return rows[i]; }
+    const vec<ncols>& operator[](const int i) const { assert(i>=0 && i<nrows); return rows[i]; }
 
     double det() const{
-        assert(rows==cols);
-        return ::det(*this);
+        assert(nrows==ncols);
+        return determinant<ncols>::det(*this);
     }
 
     double cofactor(const int row, const int col) const {
-        assert(row==col && row>=2); // rules for cofactor
-        mat<rows-1,cols-1> sub;
-        for (int i=0, subi=0; i<rows; i++) {
-            if (i == row) continue;
-            for (int j=0, subj=0; j<cols; j++) {
-                if (j == col) continue;
-                sub[subi][subj] = data[i][j];
-                subj++;
+        mat<nrows-1,ncols-1> submatrix;
+        for(int i=0; i<nrows-1; i++){
+            for(int j=0; j<ncols-1; j++){
+                submatrix[i][j] = rows[i+int(i>=row)][j+int(j>=col)]; //not included row or column of the cofactor coordinates
             }
-            subi++;
         }
-        return ((row+col)%2==0 ? 1 : -1) * sub.det();
+        return submatrix.det() * ((row+col)%2 ? -1 : 1);
     }
 
-    mat<rows,cols> invert_transpose() const {
-        mat<rows,cols> a_transpose;
-        for (int i=0; i<rows; i++)
-            for (int j=0; j<cols; j++) a_transpose[i][j]=cofactor(i,j);
-        return a_transpose/(data[0] * a_transpose[0]);// equivalent to det()
+    mat<nrows,ncols> invert_transpose() const {
+        mat<nrows,ncols> a_transpose;
+        for (int i=0; i<nrows; i++)
+            for (int j=0; j<ncols; j++) a_transpose[i][j]=cofactor(i,j);
+        return a_transpose/(rows[0] * a_transpose[0]);// equivalent to det()
     }
 
-    mat<rows,cols> invert() const {
+    mat<nrows,ncols> invert() const {
         return invert_transpose().transpose();
     }
 
-    mat<cols,rows> transpose() const {
-        mat<cols,rows> result;
-        for (int i=0; i<cols; i++){
-            for (int j=0; j<rows; j++)
-                result[i][j] = data[j][i];
+    mat<ncols,nrows> transpose() const {
+        mat<ncols,nrows> result;
+        for (int i=0; i<ncols; i++){
+            for (int j=0; j<nrows; j++)
+                result[i][j] = rows[j][i];
         }
         return result;
     }
 };
 
-template<int rows, int cols> vec<rows> operator*(const mat<rows,cols>& m, const vec<cols>& v) {
-    vec<rows> result;
-    for (int i=0; i<rows; i++)
-        result[i] = m[i] * v;
+template<int nrows, int ncols> vec<nrows> operator*(const mat<nrows,ncols>& m, const vec<ncols>& v) {
+    vec<nrows> result;
+    for (int i=0; i<nrows; i++){
+        int sum = 0;
+        for(int j=0; j<ncols; j++){
+            sum+= m[i][j] * v[j];
+        }
+        result[i] = sum;
+    }
     return result;
 }
 
-template<int cols> mat<cols,cols> operator*(const vec<cols>& lhs, const mat<1,cols>& rhs) {
-    mat<cols,cols> result;
-    for (int i=0; i<cols; i++)
-        for (int j=0; j<cols; j++)
+template<int ncols> mat<ncols,ncols> operator*(const vec<ncols>& lhs, const mat<1,ncols>& rhs) {
+    mat<ncols,ncols> result;
+    for (int i=0; i<ncols; i++)
+        for (int j=0; j<ncols; j++)
             result[i][j] = lhs[i] * rhs[j];
     return result;
 }
@@ -162,46 +164,59 @@ template<int r1, int c1, int c2> mat<r1,c2> operator*(const mat<r1,c1>& lhs, con
     return result;
 }
 
-template<int rows, int cols> mat<rows,cols> operator*(const mat<rows,cols>& lhs, const double rhs) {
-    mat<rows,cols> result;
-    for (int i=0; i<rows; i++)
-        for (int j=0; j<cols; j++)
+template<int nrows, int ncols> mat<nrows,ncols> operator*(const mat<nrows,ncols>& lhs, const double rhs) {
+    mat<nrows,ncols> result;
+    for (int i=0; i<nrows; i++)
+        for (int j=0; j<ncols; j++)
             result[i][j] = lhs[i][j] * rhs;
     return result;
 }
 
-template<int rows, int cols> mat<rows,cols> operator/(const double lhs, const mat<rows,cols>& rhs) {
-    mat<rows,cols> result;
-    for (int i=0; i<rows; i++)
-        for (int j=0; j<cols; j++)
-            result[i][j] = rhs[i][j] / lhs;
+template<int nrows, int ncols> mat<nrows,ncols> operator/(const mat<nrows,ncols>& lhs, const double rhs) {
+    mat<nrows,ncols> result;
+    for (int i=0; i<nrows; i++)
+        for (int j=0; j<ncols; j++)
+            result[i][j] = lhs[i][j] / rhs;
     return result;
 }
 
-template<int rows, int cols> mat<rows,cols> operator+(const mat<rows,cols>& lhs, const mat<rows,cols>& rhs) {
-    mat<rows,cols> result;
-    for (int i=0; i<rows; i++)
-        for (int j=0; j<cols; j++)
+template<int nrows, int ncols> mat<nrows,ncols> operator+(const mat<nrows,ncols>& lhs, const mat<nrows,ncols>& rhs) {
+    mat<nrows,ncols> result;
+    for (int i=0; i<nrows; i++)
+        for (int j=0; j<ncols; j++)
             result[i][j] = lhs[i][j] + rhs[i][j];
     return result;
 }
 
-template<int rows, int cols> mat<rows,cols> operator-(const mat<rows,cols>& lhs, const mat<rows,cols>& rhs) {
-    mat<rows,cols> result;
-    for (int i=0; i<rows; i++)
-        for (int j=0; j<cols; j++)
+template<int nrows, int ncols> mat<nrows,ncols> operator-(const mat<nrows,ncols>& lhs, const mat<nrows,ncols>& rhs) {
+    mat<nrows,ncols> result;
+    for (int i=0; i<nrows; i++)
+        for (int j=0; j<ncols; j++)
             result[i][j] = lhs[i][j] - rhs[i][j];
     return result;
 }
 
-template<int n> double det(const mat<n,n>& src) { //recursive determinant calculation
-    if (n == 1){
+template<int nrows,int ncols> std::ostream& operator<<(std::ostream& out, const mat<nrows,ncols>& m) {
+    for (int i=0; i<nrows; i++) out << m[i] << std::endl;
+    return out;
+}
+
+template<int n> struct determinant {
+    static double det(const mat<n,n>& src) { //recursive determinant calculation
+        // if (n == 1){
+        //     return src[0][0]; //need to figure out when i get wifi againnnnn
+        // }
+        double ret = 0;
+        for (int i=0; i<n; i++){
+            ret += src[0][i] * src.cofactor(0,i);
+        }
+        return ret;
+    }
+};
+
+template<> struct determinant<1> {
+    static double det(const mat<1,1>& src) {
         return src[0][0];
     }
-    double ret = 0;
-    for (int i=0; i<n; i++){
-        ret += src[0][i] * src.cofactor(0,i)
-    }
-    return ret;
-}
+};
 
